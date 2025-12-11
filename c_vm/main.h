30b4635613291966d64718a61353338e2fd5800e
dekuNukem <dekunukem@gmail.com>
2025-12-11 22:51:23 +0000

    autogen opcodes c

--- c_vm/main.h
+++ c_vm/main.h
@@ -5,82 +5,20 @@
 #include <stdint.h>
 #include <string.h>
 
-#define OP_NOP 0
-#define OP_PUSHC16 1
-#define OP_PUSHI32 2
-#define OP_POP 3
-#define OP_BRZ 4
-#define OP_JMP 5
-#define OP_CALL 6
-#define OP_RET 7
-#define OP_HALT 8
-#define OP_EQ 9
-#define OP_NOTEQ 10
-#define OP_LT 11
-#define OP_LTE 12
-#define OP_GT 13
-#define OP_GTE 14
-#define OP_ADD 15
-#define OP_SUB 16
-#define OP_MULT 17
-#define OP_DIV 18
-#define OP_MOD 19
-#define OP_POW 20
-#define OP_LSHIFT 21
-#define OP_RSHIFT 22
-#define OP_BITOR 23
-#define OP_BITAND 24
-#define OP_LOGIAND 25
-#define OP_LOGIOR 26
-#define OP_DELAY 27
-#define OP_KUP 28
-#define OP_KDOWN 29
-#define OP_MSCL 30
-#define OP_MMOV 31
-#define OP_SWCF 32
-#define OP_SWCC 33
-#define OP_SWCR 34
-#define OP_STR 35
-#define OP_STRLN 36
-// #define OP_EMUK 37
-#define OP_OLC 38
-#define OP_OLP 39
-#define OP_OLU 40
-#define OP_OLB 41
-#define OP_OLR 42
-#define OP_BCLR 43
-#define OP_PREVP 44
-#define OP_NEXTP 45
-#define OP_GOTOP 46
-#define OP_SLEEP 47
-
-#define OP_OLED_LINE 48
-#define OP_OLED_RECT 49
-#define OP_OLED_CIRCLE 50
-
-#define OP_BITXOR 51
-
-
-#define OP_VMINFO 255
-
 //--------------
 
 #define EXE_BIN_START_ADDRESS 0x0
 
 #define DATA_STACK_START_ADDRESS 0xf800
-#define DATA_STACK_BYTE_WIDTH 4
 
 #define USER_VAR_START_ADDRESS 0xFA00
-#define USER_VAR_BYTE_WIDTH 4
 #define USER_VAR_END_ADDRESS_INCLUSIVE 0xFBFF
 
 #define PGV_COUNT 32
-#define PGV_START_ADDRESS 0xFD80
-#define PGV_BYTE_WIDTH 4
+#define PGV_START_ADDRESS 0xFD00
 #define PGV_END_ADDRESS_INCLUSIVE 0xFDFF
 
 #define INTERAL_VAR_START_ADDRESS 0xFE00
-#define INTERAL_VAR_BYTE_WIDTH 4
 #define INTERAL_VAR_END_ADDRESS_INCLUSIVE 0xFFFF
 
 // ----------
