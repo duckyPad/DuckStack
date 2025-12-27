0d0cca923114dd96cb2f64faf5b0ac5851696e45
dekuNukem <dekunukem@gmail.com>
2025-12-27 11:51:47 +0000

    drops ALLOC 0

--- ds_compiler/dsvm_make_bytecode.py
+++ ds_compiler/dsvm_make_bytecode.py
@@ -420,8 +420,7 @@ def compile_to_bin(rdict):
     that means if a function has args but none are referenced, those args wont show up
     """
     user_strings_dict = {}
-
-    func_arg_and_local_var_lookup = group_vars(rdict)
+    func_arg_and_local_var_lookup = rdict['func_arg_and_local_var_lookup']
     user_declared_global_var_addr_lookup = {}
     for index, item in enumerate(sorted([x.name for x in rdict['var_info_set'] if x.type is SymType.GLOBAL_VAR])):
         user_declared_global_var_addr_lookup[item] = index * USER_VAR_BYTE_WIDTH + USER_VAR_START_ADDRESS
@@ -537,7 +536,7 @@ def compile_to_bin(rdict):
         raise ValueError("Binary size too large")
     return output_bin_array
 
-def optimize_pass(instruction_list):
+def optimize_pass(instruction_list, arg_and_var_dict):
     optimized_list = []
     i = 0
     while i < len(instruction_list):
@@ -547,12 +546,12 @@ def optimize_pass(instruction_list):
         if i + 1 < len(instruction_list):
             next_instr = instruction_list[i + 1]
             
-            # 1. Optimize: PUSH0 + DROP -> Remove both
+            # PUSH0 + DROP -> Remove both
             if current_instr.opcode == OP_PUSH0 and next_instr.opcode == OP_DROP:
                 i += 2
                 continue
 
-            # 2. Optimize: POPI/POPR [X] + PUSHI/PUSHR [X] -> DUP + POPI/POPR [X]
+            # POPI/POPR [X] + PUSHI/PUSHR [X] -> DUP + POPI/POPR [X]
             # This handles both Global (POPI) and Local (POPR) variables
             is_same_mem_save_load = (current_instr.opcode == OP_POPI and next_instr.opcode == OP_PUSHI) or (current_instr.opcode == OP_POPR and next_instr.opcode == OP_PUSHR)
             if is_same_mem_save_load and current_instr.payload == next_instr.payload:
@@ -561,9 +560,12 @@ def optimize_pass(instruction_list):
                 i += 2
                 continue
 
-        # 3. Optimize: PUSHC16 0 -> PUSH0
+        # PUSHC16 0 -> PUSH0
         if current_instr.opcode == OP_PUSHC16 and current_instr.payload == 0:
             optimized_list.append(dsvm_instruction(opcode=OP_PUSH0, label=current_instr.label, comment=current_instr.comment))
+            i += 1
+            continue
+        if current_instr.opcode == OP_ALLOC and current_instr.payload in arg_and_var_dict and len(arg_and_var_dict[current_instr.payload]['locals']) == 0:
             i += 1
             continue
 
@@ -573,9 +575,10 @@ def optimize_pass(instruction_list):
     return optimized_list
 
 def optimize_full_assembly_from_context_dict(ctx_dict):
-    ctx_dict["root_assembly_list"] = optimize_pass(ctx_dict["root_assembly_list"])
+    arg_and_var_dict = ctx_dict['func_arg_and_local_var_lookup']
+    ctx_dict["root_assembly_list"] = optimize_pass(ctx_dict["root_assembly_list"], arg_and_var_dict)
     for key in ctx_dict['func_assembly_dict']:
-        ctx_dict['func_assembly_dict'][key] = optimize_pass(ctx_dict['func_assembly_dict'][key])
+        ctx_dict['func_assembly_dict'][key] = optimize_pass(ctx_dict['func_assembly_dict'][key], arg_and_var_dict)
 
 def replace_dummy_with_drop(instruction_list):
     for this_instruction in instruction_list:
@@ -639,6 +642,7 @@ def make_dsb_with_exception(program_listing, should_print=False):
 
     print("\n\n--------- Assembly Listing, Unoptimised, Unresolved: ---------")
     print_full_assembly_from_context_dict(rdict)
+    rdict['func_arg_and_local_var_lookup'] = group_vars(rdict)
 
     replace_dummy_with_drop_from_context_dict(rdict)
     optimize_full_assembly_from_context_dict(rdict)
